e6b11abfb19eac4644c2383af63d7ddd7bb142e5
onyx <onyxceo1@gmail.com>
2025-07-20 20:24:01 +0300

    MSW for online

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-// Start MSW in development
-if (process.env.NODE_ENV === 'development') {
+// Function to enable mocking
+async function enableMocking() {
+  // This is the important part!
+  // It checks for the Vercel environment variable.
+  if (import.meta.env.VITE_API_MOCKING !== 'enabled') {
+    return;
+  }
+
   const { worker } = await import('./mocks/browser');
-  worker.start({
+  // Start the worker
+  return worker.start({
     onUnhandledRequest: 'bypass',
   });
 }
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Enable mocking before rendering the app
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
